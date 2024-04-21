89a5c1b55367563f03eb29ceac7d3e313ca8579c
Yaandle <131413645+Yaandle@users.noreply.github.com>
2024-04-21 10:55:51 +1000

    Update imagepredict.py

--- imagepredict.py
+++ imagepredict.py
@@ -4,8 +4,8 @@ import torchvision
 import time
 import os
 
-source = 'E:\odis\odistesting\Testing\dataset\imaegimages' 
-model = YOLO("E:\odis\odistesting\Testing\models\Model4600.pt")
+source = 'E:\Testing\dataset\imaegimages' 
+model = YOLO("\Testing\models\Model4600.pt")
 
 
 image_files = [f for f in os.listdir(source) if f.endswith(('.jpg', '.jpeg', '.png'))]
